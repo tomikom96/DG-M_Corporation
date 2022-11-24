fc2f8eac322bf4128376317fba549d1223ad4b52
Tomasz <tomasz@siwinski@o2.pl>
2022-11-24 18:43:41 +0100

    added styles and modified javascript

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ const navMobile = document.querySelector(".nav-mobile");
 const navBtn = document.querySelector(".hamburger");
 const navFooterYear = document.querySelector(".footer__year");
 
+const allNavItems = document.querySelectorAll(".nav__link");
+
 const handleNav = () => {
 	navBtn.classList.toggle("is-active");
 	navMobile.classList.toggle("nav-mobile--active");
+
+	allNavItems.forEach(item => {
+		item.addEventListener("click", () => {
+			navMobile.classList.remove("nav-mobile--active");
+			navBtn.classList.remove("is-active");
+		});
+	});
 };
 
 navBtn.addEventListener("click", handleNav);
